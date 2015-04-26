d76fed9a1227a5a23ec14ace2563e7354d023d3f
James McShane <jmcshan1@gmail.com>
2015-04-26 14:50:32 -0400

    Configuring sonarRunner within the task rather than the plugin and parsing JSON

--- src/main/groovy/com/mcshane/gradle/istanbul/IstanbulExtension.groovy
+++ src/main/groovy/com/mcshane/gradle/istanbul/IstanbulExtension.groovy
@@ -1,11 +1,39 @@
 package com.mcshane.gradle.istanbul
 
+import groovy.json.JsonSlurper
+
 class IstanbulExtension {
 
 	static final String ISTANBUL_EXTENSION_NAME = "istanbul";
 	static final String ISTANBUL_TASK_NAME = "istanbul";
 	
+	private boolean jsonParsed
+	
 	public String json
 	public String taskName
 	public String coverageFile
+	
+	def parseJson() {
+		def jsonFile = new File(json).text
+		def properties = new JsonSlurper().parseText(jsonFile)
+		this.taskName = properties.istanbul_grunt_task
+		this.coverageFile = properties.istanbul_coverage_dir
+		jsonParsed = true
+	}
+
+	def String getTaskName() {
+		parseIfNecessary()
+		return taskName
+	}
+	
+	def String getCoverageFile() {
+		parseIfNecessary()
+		return coverageFile
+	}
+	
+	private void parseIfNecessary() {
+		if (json != null && !jsonParsed) {
+			parseJson()
+		}
+	}
 }
--- src/main/groovy/com/mcshane/gradle/istanbul/IstanbulTask.groovy
+++ src/main/groovy/com/mcshane/gradle/istanbul/IstanbulTask.groovy
@@ -8,8 +8,7 @@ class IstanbulTask extends DefaultTask {
 
 	@TaskAction
 	public void istanbul() {
-		IstanbulExtension extension = 
-			project.getExtensions().getByName(IstanbulExtension.ISTANBUL_EXTENSION_NAME)
+		IstanbulExtension extension = project.extensions[IstanbulExtension.ISTANBUL_EXTENSION_NAME]
 		if (project.file(extension.getCoverageFile()).exists()) {
 			project.plugins.withId('sonarRunner') {
 				configureSonarRunner(extension)
@@ -19,7 +18,7 @@ class IstanbulTask extends DefaultTask {
 	
 	private void configureSonarRunner(IstanbulExtension extension) {
 		SonarRunnerExtension sonarRunner = 
-			project.getExtensions().getByName(SonarRunnerExtension.SONAR_RUNNER_EXTENSION_NAME)
+			project.extensions[SonarRunnerExtension.SONAR_RUNNER_EXTENSION_NAME]
 		sonarRunner.sonarProperties {
 			property "sonar.javascript.lcov.reportPath" "${extension.getCoverageFile()}"
 		}
